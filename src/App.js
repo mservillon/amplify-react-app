adfc2d9fd24ea215cc13e0a2847bc9b840752278
mservillon <mackservillon@gmail.com>
2023-10-17 18:42:50 -0500

    Updating the ui to specify start and limit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,15 @@ function App() {
   
   return (
     <div className="App">
+      <input
+          placeholder="start"
+          onChange={e => updateInputValues('start', e.target.value)}  
+      />
+      <input
+          onChange={e => updateInputValues('limit', e.target.value)}
+          placeholder="limit"
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
