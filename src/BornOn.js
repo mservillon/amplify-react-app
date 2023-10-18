2176914dc058640d3017371b97258c30f4889adc
mservillon <mackservillon@gmail.com>
2023-10-17 22:45:12 -0500

    Added a few things
    I wanted to add a loading useState so that way it doesn't look too awkward when the /born API is being called. Honestly thought it would be a cool feature.
    
    I also wanted to make the date human readable so I decided to format the date.

--- src/BornOn.js
+++ src/BornOn.js
@@ -5,20 +5,32 @@ export const BornOn = () => {
 
     const [born, updateBorn] = useState([]);
 
+    const [loading, updateLoading] = useState(true);
+
     useEffect(() => {
         fetchName();
       }, []);
     
     
       const fetchName = async() => {
+        updateLoading(true);
         const nameData = await API.get('cryptoapi', '/born');
         console.log(nameData);
         updateBorn(nameData.born);
+        updateLoading(false);
       }
+
+      let date = (born.created_at);
+      let currentDate = new Date(date);
+      let month = currentDate.getMonth() + 1;
+      let day = currentDate.getDate();
+      let year = currentDate.getFullYear();
+      let prettyDateFormat = month + "/" + day + "/" + year;
 
     return (
         <>
-        <h3>The GitHub user {born.login} was made on {born.created_at}</h3>
+        {loading && <h1>Loading data for GitHub user...</h1>}
+        {!loading && <h3>The GitHub user {born.login} was made on {prettyDateFormat} !!</h3>}
         </>
     )
 }
